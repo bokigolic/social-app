a6a08f1b9095a209f93b1204c44d599c40042c34
bokigolic <bokigolic32@gmail.com>
2022-09-01 13:02:01 -0400

    axios works

---
+++ README_MY.md
@@ -0,0 +1,12 @@
+
+
+# HOW TO START json-server backend
+
+https://www.npmjs.com/package/json-server#alternative-port
+
+json-server --watch db.json --port 3033
+
+(you must be in folder \social-app\json-server-db )
+
+then check in your browser, for example:
+http://localhost:3033/posts
--- social-app-react/src/components/App.jsx
+++ social-app-react/src/components/App.jsx
@@ -1,7 +1,36 @@
+import { useEffect } from "react";
+import axios from 'axios';
 import "../scss/style.scss";
 
 
 const App = () => {
+  console.log('testing backend...');
+
+  useEffect(() => {
+    axios.get('http://localhost:3033/posts')
+      .then((response) => {
+        console.log('response GET', response)
+      })
+
+    const data = {
+      title: 'treci'
+    };
+    axios.post('http://localhost:3033/posts', data)
+      .then((response) => {
+        console.log('response POST', response)
+      })
+
+    const data2 = {
+      username: 'boki',
+      password: 'boki'
+    };
+    axios.post('http://localhost:3033/users', data2)
+      .then((response) => {
+        console.log('response POST 2', response)
+      })
+
+  }, []);
+
   return (
     <div className="App">
       App
